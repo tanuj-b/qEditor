8944617e5a58b975b2d23046cb5126d3729102ca
Tanuj Bhojwani <tanuj.bhojwani@gmail.com>
2013-02-12 20:39:14 +0530

    Question adding and editing now working.

--- js/views/questionviews.js
+++ js/views/questionviews.js
@@ -1,30 +1,75 @@
 window.views["questionAdd"] = Backbone.View.extend({
 
+	initialize:function () {
+        this.render();
+	    this.editor = [];
+	    this.list = ['text',
+	    	'opt-1',
+	        'opt-2',
+	        'opt-3',
+	        'opt-4',
+	        'explanation'];
+	    var editor = [];
+        _.each(this.list,function(item){
+        	editor[item] = CKEDITOR.replace(item);
+        });
+        this.editor = editor;
+    },
+
+    events: {
+        'click .edit': 'edit',
+        'click .preview': 'preview',
+        'click #submit' : 'submit'
+    },
+
+    edit: function(e){
+    	id = ($(e.target).attr("id")).substr(2);
+    	this.editor[id] = CKEDITOR.replace(id);
+    },
+
+    preview: function(e){
+    	id = ($(e.target).attr("id")).substr(2);
+    	var context = this;
+    	if(id=="all")
+    		_.each(this.list,function(item){
+    			data = context.editor[item].getData();
+	    		context.editor[item].destroy();
+	    		$(item).html(data);		
+    		});
+		data = this.editor[id].getData();
+    	this.editor[id].destroy();
+    	$(id).html(data);
+    },
+
+    submit:  function(e){
+    	var data = [];
+    	_.each(this.list,function(item){
+    		data[item] = context.editor[item].getData();
+    	});
+    	//ajax call to server
+    },
+
+    render:function () {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+window.views["questionEdit"] = Backbone.View.extend({
+
 	defaults : {
-		editor : null
 	},
 
 
 	initialize:function () {
         this.render();
-        CKEDITOR.inline('#text');
-        CKEDITOR.inline('#opt-1');
-        CKEDITOR.inline('#opt-2');
-        CKEDITOR.inline('#opt-3');
-        CKEDITOR.inline('#opt-4');
-        CKEDITOR.inline('#explanation');
     },
 
     events: {
-        'click #submit': 'submit',
-        'click #preview': 'preview',
+        'click #submit': 'submit'
     },
 
     submit: function(){
-
-    },
-
-    preview: function(){
 
     },
 
